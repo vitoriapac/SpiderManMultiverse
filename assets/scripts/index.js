969e7182e7bdfb0962dd70d52b875ffe521188db
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-09-30 22:36:04 -0300

    Card carousel

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,26 +1,26 @@
 function handleMouseEnter() {
-  this.classList.add('s-card--hovered');
-  document.body.id = `${this.id}-hovered`;
+  this.classList.add('s-card--hovered')
+  document.body.id = `${this.id}-hovered`
 }
 
 function handleMouseLeave() {
-  this.classList.remove('s-card--hovered');
-  document.body.id = '';
+  this.classList.remove('s-card--hovered')
+  document.body.id = ''
 }
 
 function addEventListenersToCards() {
-  const cardElements = document.getElementsByClassName('s-card');
-  
+  const cardElements = document.getElementsByClassName('s-card')
+
   for (let index = 0; index < cardElements.length; index++) {
-    const card = cardElements[index];
-    card.addEventListener('mouseenter', handleMouseEnter);
-    card.addEventListener('mouseleave', handleMouseLeave);
+    const card = cardElements[index]
+    card.addEventListener('mouseenter', handleMouseEnter)
+    card.addEventListener('mouseleave', handleMouseLeave)
   }
 }
 
-document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
+document.addEventListener('DOMContentLoaded', addEventListenersToCards, false)
 
-function selectCarouselItem(selectedButtonElement) {
+/*function selectCarouselItem(selectedButtonElement) {
   const selectedItem = selectedButtonElement.id;
   const carousel = document.querySelector('.s-cards-carousel');
   const transform = carousel.style.transform;
@@ -33,4 +33,4 @@ function selectCarouselItem(selectedButtonElement) {
   const activeButtonElement = document.querySelector('.s-controller__button--active');
   activeButtonElement.classList.remove('s-controller__button--active');
   selectedButtonElement.classList.add('s-controller__button--active');
-}
+}*/
